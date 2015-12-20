27a9484826015377c7a9400b16640e5b5e65b83a
pawc <pswid89@gmail.com>
2015-12-20 19:19:47 +0100

    initial commit

---
+++ Client/src/test/java/pl/pawc/Remote/ClientTest.java
@@ -0,0 +1,19 @@
+package pl.pawc.Remote;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class ClientTest extends TestCase{
+    public ClientTest(String testName){
+        super(testName);
+    }
+
+    public static Test suite(){
+        return new TestSuite(ClientTest.class);
+    }
+
+    public void testClient(){
+        assertTrue(true);
+    }
+}
---
+++ Server/src/test/java/pl/pawc/Remote/ServerTest.java
@@ -0,0 +1,19 @@
+package pl.pawc.Remote;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class ServerTest extends TestCase{
+    public ServerTest(String testName){
+        super(testName);
+    }
+
+    public static Test suite(){
+        return new TestSuite(ServerTest.class);
+    }
+
+    public void testServer(){
+        assertTrue(true);
+    }
+}
